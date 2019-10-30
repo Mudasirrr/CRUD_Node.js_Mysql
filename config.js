9d1be0b8f833a0d7d9eed54115ee4a94cf3e7428
Mudasir Hussain <hmudasir714@gmail.com>
2019-10-30 05:00:52 -0700

    First commit

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {
+	database: {
+		host:	  'localhost', 	// database host
+		user: 	  'root', 		// your database username
+		password: '', 		// default MySQL port
+		db:'test' 		// your database name
+	}
+}
+
+module.exports = config
